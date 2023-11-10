7d60bb0bc6a3e36f26882b339f04213c68fa72c7
Charles Wilmot <chazwilmot@gmail.com>
2023-11-10 11:40:20 -0600

    step def: listings and settings tab

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -13,3 +13,9 @@ end
 And(/^I should see the user profile information$/) do
   expect(page).to have_css('.user-info')
 end
+
+And(/^I should see tabs for Listings and Settings$/) do
+  expect(page).to have_css('.nav-tabs')
+  expect(page).to have_link('Listings')
+  expect(page).to have_link('Settings')
+end
