88c12c3dd9a14d87363b961dd687209a72e45bb4
Charles Wilmot <chazwilmot@gmail.com>
2023-11-10 11:41:49 -0600

    step def: email, password, login

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,3 +1,9 @@
 Given(/^I am on the login page$/) do
   visit 'user/login' # Replace with the correct path
 end
+
+When(/^I enter valid credentials$/) do
+  fill_in 'Email', with: 'user@example.com' # Replace with actual field names
+  fill_in 'Password', with: 'password'
+  click_button 'Login'
+end
