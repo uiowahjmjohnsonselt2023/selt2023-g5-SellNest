5a2e93110aa59cb9361f590e0c7fdf036033f923
Charles Wilmot <chazwilmot@gmail.com>
2023-11-10 11:43:18 -0600

    step def: signup forms

--- features/step_definitions/signup_steps.rb
+++ features/step_definitions/signup_steps.rb
@@ -1,3 +1,10 @@
 Given(/^I am on the signup page$/) do
   visit 'user/signup' # Replace with the correct path
 end
+
+When(/^I fill in the signup form$/) do
+  fill_in 'Username', with: 'newuser'
+  fill_in 'Email', with: 'newuser@example.com'
+  fill_in 'Password', with: 'password'
+  fill_in 'Password confirmation', with: 'password'
+end
