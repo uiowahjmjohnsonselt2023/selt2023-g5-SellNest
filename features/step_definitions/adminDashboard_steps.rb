198068da49055f66d45dba6b94f36b91eabd97f1
Charles Wilmot <chazwilmot@gmail.com>
2023-11-10 11:33:00 -0600

    Cucumber Step Def: Total Sellers
    Total Sellers Stat

--- features/step_definitions/adminDashboard_steps.rb
+++ features/step_definitions/adminDashboard_steps.rb
@@ -19,3 +19,10 @@ And(/^I should see the total users stat$/) do
     expect(page).to have_css('.stat-value', text: /\d+/) # Check if a number is displayed
   end
 end
+
+And(/^I should see the total sellers stat$/) do
+  within('.dashboard-stats') do
+    expect(page).to have_content('Total Sellers')
+    expect(page).to have_css('.stat-value', text: /\d+/) # Check if a number is displayed
+  end
+end
