cd69a5ab2908265f44fa33e20e7f752be01bed8c
Charles Wilmot <chazwilmot@gmail.com>
2023-11-10 11:39:37 -0600

    step def: users photo

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,3 +1,7 @@
 Given(/^I am on the user profile page$/) do
-  visit '/user_profile' # Replace with the correct path
+  visit 'user/index' # Replace with the correct path
+end
+
+Then(/^I should see the user's photo$/) do
+  expect(page).to have_css('img.profile-photo')
 end
