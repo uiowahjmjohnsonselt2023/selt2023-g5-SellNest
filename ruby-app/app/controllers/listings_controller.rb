cbfdd864bf506b0d903379c6bf355af4e1339e40
teckenrod <treyeck2@gmail.com>
2023-11-10 21:09:59 -0600

    added basic pages for listings and functionality

--- ruby-app/app/controllers/listings_controller.rb
+++ ruby-app/app/controllers/listings_controller.rb
@@ -1,4 +1,27 @@
 class ListingsController < ApplicationController
   def edit
+    @listing = Listing.find params[:id]
+  end
+
+  def new
+  end
+
+  def listing_params
+    params.require(:listing).permit(:name, :photos, :description, :price, :sellerID)
+  end
+
+  def create
+    @listing = Listing.create!(listing_params)
+    flash[:notice] = "#{@Listing.name} was successfully created."
+    redirect_to listings_path
+  end
+
+  def index
+    @listings = Listing.all
+    render :index
+  end
+
+  def update  # does not actually update info at the moment
+    index
   end
 end
--- ruby-app/config/routes.rb
+++ ruby-app/config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
 
   get 'listings/new'
 
+
+  resources :listings
+  root :to => redirect('/listings')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
