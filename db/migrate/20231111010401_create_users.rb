5c0eec2d593862709599a51869ce1191484f7d19
Tristan Pawlenty <tristan-pawlenty@uiowa.edu>
2023-11-10 23:24:51 -0600

    Added method to upgrade a user account to seller

--- db/migrate/20231111010401_create_users.rb
+++ db/migrate/20231111010401_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :email, null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      t.boolean :seller, default: false
       t.timestamps
     end
 
