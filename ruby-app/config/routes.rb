d61aff41cff2502907553998160703cbcd8278bc
teckenrod <treyeck2@gmail.com>
2023-11-10 18:08:06 -0600

    added index and edits

--- ruby-app/config/routes.rb
+++ ruby-app/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'listings/edit'
+
+  get 'listings/index'
+
   get 'listings/new'
 
   # The priority is based upon order of creation: first created -> highest priority.
