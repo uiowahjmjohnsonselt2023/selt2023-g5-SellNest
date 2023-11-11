0edb08cbdd10504026286e529fa34617cf0bf65a
Tristan Pawlenty <tristan-pawlenty@uiowa.edu>
2023-11-10 20:02:43 -0600

    testing database

---
+++ db/migrate/20231111012247_create_listings.rb
@@ -0,0 +1,13 @@
+class CreateListings < ActiveRecord::Migration
+  def change
+    create_table :listings do |t|
+      t.string :name
+      t.text :description
+      t.decimal :price
+      t.string :photoUrl
+      t.references :sellerID
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20231111011719) do
+ActiveRecord::Schema.define(version: 20231111012247) do
+
+  create_table "listings", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.decimal  "price"
+    t.string   "photoUrl"
+    t.integer  "sellerID_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
