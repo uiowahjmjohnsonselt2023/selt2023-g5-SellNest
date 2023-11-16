b6db9f0c1987b9c87c6d2b21e9ccb4d17868d77b
Tristan Pawlenty <tristan-pawlenty@uiowa.edu>
2023-11-15 22:40:11 -0600

    wrote the session controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,3 +1,13 @@
 class SessionController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
 
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id # Set the user ID in the session
+      redirect_to root_path, notice: "Logged in successfully!"
+    else
+      flash.now[:alert] = "Invalid email or password"
+      render :new
+    end
+  end
 end
