1a8738ba90cd9fee2aff93c22678445fa64a3cb0
teckenrod <treyeck2@gmail.com>
2023-11-22 12:37:27 -0600

    photos added in listings table

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,8 @@
 class Listing < ActiveRecord::Base
-  belongs_to :user
-  # has_many_attached :photos
+  # belongs_to :user
+  has_many_attached :photos
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
