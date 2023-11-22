0326df85ff45a20991ce08e937abf3ba8bb536d8
Tristan Pawlenty <tristan-pawlenty@uiowa.edu>
2023-11-22 17:22:46 -0600

    New schema file from the two new tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_11_013300) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_22_231743) do
+  create_table "listing_tags", force: :cascade do |t|
+    t.integer "tags_id"
+    t.integer "listings_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["listings_id"], name: "index_listing_tags_on_listings_id"
+    t.index ["tags_id"], name: "index_listing_tags_on_tags_id"
+  end
+
   create_table "listings", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -20,6 +29,12 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_11_013300) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_listings_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "tag"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,5 +50,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_11_013300) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "listing_tags", "listings", column: "listings_id"
+  add_foreign_key "listing_tags", "tags", column: "tags_id"
   add_foreign_key "listings", "users"
 end
