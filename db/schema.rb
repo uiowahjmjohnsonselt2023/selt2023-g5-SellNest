e2b7717b0a12723a04b671317eee2f13c8f60714
Tristan Pawlenty <tristan-pawlenty@uiowa.edu>
2023-11-23 10:29:12 -0600

    ran new migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_22_231743) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_23_020214) do
   create_table "listing_tags", force: :cascade do |t|
-    t.integer "tags_id"
-    t.integer "listings_id"
+    t.integer "listing_id", null: false
+    t.integer "tag_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["listings_id"], name: "index_listing_tags_on_listings_id"
-    t.index ["tags_id"], name: "index_listing_tags_on_tags_id"
+    t.index ["listing_id"], name: "index_listing_tags_on_listing_id"
+    t.index ["tag_id"], name: "index_listing_tags_on_tag_id"
   end
 
   create_table "listings", force: :cascade do |t|
@@ -32,7 +32,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_231743) do
   end
 
   create_table "tags", force: :cascade do |t|
-    t.string "tag"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -50,7 +50,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_231743) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "listing_tags", "listings", column: "listings_id"
-  add_foreign_key "listing_tags", "tags", column: "tags_id"
+  add_foreign_key "listing_tags", "listings"
+  add_foreign_key "listing_tags", "tags"
   add_foreign_key "listings", "users"
 end
