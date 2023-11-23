31b74ba285c4ad0e82816abb4118e3ea92efbcfa
Tristan Pawlenty <tristan-pawlenty@uiowa.edu>
2023-11-22 19:51:04 -0600

    Fixed seeding with model changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-tags = Tag.create([{tag: 'Art & Crafts'}, {tag: 'Beauty & Personal Care'}, {tag: 'Books'}, {tag: 'Clothing'},
-                   {tag: 'Electronics'}, {tag: 'Furniture'}, {tag: 'Health & Fitness'}, {tag: 'Home & Kitchen'},
-                   {tag: 'Sports & Outdoors'}, {tag: 'Toys & Games'}])
+tags = Tag.create([{ name: 'Art & Crafts'}, { name: 'Beauty & Personal Care'}, { name: 'Books'}, { name: 'Clothing'},
+                   { name: 'Electronics'}, { name: 'Furniture'}, { name: 'Health & Fitness'}, { name: 'Home & Kitchen'},
+                   { name: 'Sports & Outdoors'}, { name: 'Toys & Games'}])
