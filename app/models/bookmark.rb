f0eb708e692dc6cded8db18661d31018b3ab1cd4
Charles Wilmot II <chazwilmot@gmail.com>
2023-11-24 10:08:52 -0800

    bookmark.rb

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,3 +2,9 @@ class Bookmark < ActiveRecord::Base
   belongs_to :user
   belongs_to :listable, polymorphic: true
 end
+
+# User needs to
+# has_many :bookmarks
+
+# Listing needs to
+# has_many :bookmarks, as: :listable
