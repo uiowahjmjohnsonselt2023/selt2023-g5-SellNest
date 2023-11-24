2b41ca73390ef2a85a93671a37ed91762c4a0efc
Charles Wilmot II <chazwilmot@gmail.com>
2023-11-24 10:06:51 -0800

    starter code for def create bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,11 @@
 class BookmarksController < ApplicationController
+  def create
+    # temp code
+    # @bookmark = current_user.bookmarks.build(bookmark_params)
+    #     if @bookmark.save
+    #       # respond with success message or update UI accordingly
+    #     else
+    #       # respond with error message
+    #     end
+  end
 end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,4 @@
-class Bookmark < ApplicationRecord
+class Bookmark < ActiveRecord::Base
   belongs_to :user
   belongs_to :listable, polymorphic: true
 end
