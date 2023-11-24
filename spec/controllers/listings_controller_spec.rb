eb5d16d161f1f324eadbd63259dcc24180cd49f3
Charles Wilmot II <chazwilmot@gmail.com>
2023-11-24 10:56:15 -0800

    listings controller spec

--- spec/controllers/listings_controller_spec.rb
+++ spec/controllers/listings_controller_spec.rb
@@ -1,3 +1,20 @@
-describe ListingsController do
+require 'rails_helper'
 
+RSpec.describe ListingsController, type: :controller do
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'creates a new listing and redirects' do
+        post :create, params: { listing: { title: 'Charming Apartment', description: 'Spacious and modern', price: 1200 } }
+        expect(response).to redirect_to(Listing.last)
+        expect(flash[:notice]).to eq('Listing was successfully created.')
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not create a new listing' do
+        post :create, params: { listing: { description: 'Spacious and modern', price: 1200 } }
+        expect(Listing.count).to eq(0)
+      end
+    end
+  end
 end
