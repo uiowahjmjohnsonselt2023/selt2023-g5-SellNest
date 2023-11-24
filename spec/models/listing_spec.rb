92ec28f425444ac9c005696dd82ba3ef00288230
teckenrod <treyeck2@gmail.com>
2023-11-24 17:57:34 -0600

    testing

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Listing, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is not valid without a name" do
+    listing = Listing.new(description: 'Some description', price: 100)
+    expect(listing).to_not be_valid
+  end
+
+  it "is not valid without a description" do
+    listing = Listing.new(name: 'Example Listing', price: 100)
+    expect(listing).to_not be_valid
+  end
+
+  it "is not valid without a price" do
+    listing = Listing.new(name: 'Example Listing', description: 'Some description')
+    expect(listing).to_not be_valid
+  end
+
+  it "is valid with valid attributes" do
+    listing = Listing.new(name: 'Example Listing', description: 'Some description', price: 100)
+    expect(listing).to be_valid
+  end
 end
+
