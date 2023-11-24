8beebddcc49a4dc177b86476ddcb70825859c408
Charles Wilmot II <chazwilmot@gmail.com>
2023-11-24 10:56:25 -0800

    listing model spec

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Listing, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it 'is valid with valid attributes' do
+      listing = Listing.new(title: 'Charming Apartment', description: 'Spacious and modern', price: 1200)
+      expect(listing).to be_valid
+    end
+
+    it 'is invalid without a title' do
+      listing = Listing.new(description: 'Spacious and modern', price: 1200)
+      expect(listing).not_to be_valid
+    end
+  end
 end
