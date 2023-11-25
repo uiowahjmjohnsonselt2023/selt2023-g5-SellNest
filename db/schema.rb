b5cf319262e27f3a4c609522e6ce9138cde90772
Charlize Santos <charlize-santos@uiowa.edu>
2023-11-24 22:27:37 -0600

    Secret key

--- db/schema.rb
+++ db/schema.rb
@@ -56,6 +56,10 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_182949) do
     t.boolean "seller", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "full_name"
+    t.string "uid"
+    t.string "avatar_url"
+    t.string "provider"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
