f6137598a42c892411f820f74cb235d0fe92a378
Charles Wilmot II <chazwilmot@gmail.com>
2023-12-01 00:31:52 -0600

    rails generate command for the order model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_01_055710) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_01_063121) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -76,6 +76,25 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_01_055710) do
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
+  create_table "order_items", force: :cascade do |t|
+    t.integer "order_id", null: false
+    t.integer "listing_id", null: false
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["listing_id"], name: "index_order_items_on_listing_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.decimal "total"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "tags", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -107,4 +126,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_01_055710) do
   add_foreign_key "listing_tags", "listings"
   add_foreign_key "listing_tags", "tags"
   add_foreign_key "listings", "users"
+  add_foreign_key "order_items", "listings"
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "orders", "users"
 end
