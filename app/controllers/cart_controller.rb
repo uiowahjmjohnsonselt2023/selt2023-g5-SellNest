70b04d36e7213fce2d5b06ec4efbcb0ac8bd0747
Charles Wilmot II <chazwilmot@gmail.com>
2023-12-01 00:41:47 -0600

    controller for cart updated

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,6 +1,6 @@
 class CartController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_cart, only: [:add_item, :show, :remove_item]
+  before_action :set_cart, only: [:add_item, :show, :remove_item, :checkout]
 
   def show
     @cart = current_user.cart
@@ -23,6 +23,24 @@ class CartController < ApplicationController
     cart_item = @cart.cart_items.find_or_initialize_by(listing_id: params[:listing_id])
     cart_item.quantity = (cart_item.quantity || 0) + 1
     cart_item.save
+  end
+
+  def checkout
+    if @cart.cart_items.any?
+      order = current_user.orders.create(total: @cart.total_price, status: 'complete')
+
+      @cart.cart_items.each do |item|
+        order.order_items.create(listing: item.listing, quantity: item.quantity)
+        # Here you may also want to remove the item from the cart or decrease the stock of the listing
+      end
+
+      # Clear the cart items after checkout
+      @cart.cart_items.destroy_all
+
+      redirect_to order_path(order), notice: 'Thank you for your purchase!'
+    else
+      redirect_to cart_path, alert: 'Your cart is empty.'
+    end
   end
 
   private
