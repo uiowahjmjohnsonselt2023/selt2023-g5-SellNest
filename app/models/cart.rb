d5556c92de84759206a6d4481cb5eb29303b1e08
Charles Wilmot II <chazwilmot@gmail.com>
2023-12-01 00:41:39 -0600

    model for cart updated

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,8 @@ class Cart < ApplicationRecord
   belongs_to :user
   has_many :cart_items
   has_many :listings, through: :cart_items
+
+  def total_price
+    cart_items.sum { |item| item.quantity * item.listing.price }
+  end
 end
