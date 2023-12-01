b25f161579c3da702f021ef756514a31d4fcd974
Charles Wilmot II <chazwilmot@gmail.com>
2023-12-01 14:49:19 -0600

    update listing model
    listing model needed changes to be able to be destoryed

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,11 +1,8 @@
 class Listing < ActiveRecord::Base
-
-
   has_many :listing_tags
   has_many :tags, through: :listing_tags
-  has_many :cart_items
-  has_many :order_items
-  # belongs_to :user
+  has_many :cart_items, dependent: :destroy  # Add the dependent option here
+  has_many :order_items, dependent: :destroy # And here
   has_many_attached :photos
 
   scope :search_by_title, -> (query) { where('name LIKE ?', "%#{query}%")}
