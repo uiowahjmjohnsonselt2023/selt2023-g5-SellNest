e236c518eaabacc07e9a6a94edee5613c29cd100
Charles Wilmot II <chazwilmot@gmail.com>
2023-12-01 00:00:00 -0600

    update to cart model
    has many cart items, listings, though car items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_23_020214) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_01_055710) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,23 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_23_020214) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "cart_id", null: false
+    t.integer "listing_id", null: false
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
+    t.index ["listing_id"], name: "index_cart_items_on_listing_id"
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
   create_table "listing_tags", force: :cascade do |t|
@@ -84,6 +101,9 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_23_020214) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "cart_items", "carts"
+  add_foreign_key "cart_items", "listings"
+  add_foreign_key "carts", "users"
   add_foreign_key "listing_tags", "listings"
   add_foreign_key "listing_tags", "tags"
   add_foreign_key "listings", "users"
