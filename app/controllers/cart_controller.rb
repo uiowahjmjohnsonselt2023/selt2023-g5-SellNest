5a423c4fd5b6ac2d52959c2fcebaad443db88fc9
Charles Wilmot II <chazwilmot@gmail.com>
2023-12-01 00:01:55 -0600

    update to cart controller
    show additem and removeitem functions

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,10 +1,20 @@
 class CartController < ApplicationController
+  before_action :authenticate_user!
+
   def show
+    @cart = current_user.cart
   end
 
   def add_item
+    listing = Listing.find(params[:id])
+    current_user.cart.listings << listing
+    redirect_to cart_path
   end
 
   def remove_item
+    listing = Listing.find(params[:id])
+    cart_item = current_user.cart.cart_items.find_by(listing_id: listing.id)
+    cart_item.destroy
+    redirect_to cart_path
   end
 end
