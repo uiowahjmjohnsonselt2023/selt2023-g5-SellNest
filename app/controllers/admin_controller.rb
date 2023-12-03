2a43897f305d07b57b6a11d6998f11749254bfc2
teckenrod <treyeck2@gmail.com>
2023-12-03 15:43:21 -0600

    added total sellers on admin page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,5 +2,6 @@ class AdminController < ApplicationController
   def index
     @total_users = User.count
     @items_listed = Listing.count
+    @total_sellers = User.where(seller: true).count
   end
 end
