cc2a13f1a3900c64c9d55cea5d344f60ccd44c85
teckenrod <treyeck2@gmail.com>
2023-12-03 15:48:50 -0600

    added total sales on admin page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,5 +3,6 @@ class AdminController < ApplicationController
     @total_users = User.count
     @items_listed = Listing.count
     @total_sellers = User.where(seller: true).count
+    @total_sales = Order.sum(:total)
   end
 end
