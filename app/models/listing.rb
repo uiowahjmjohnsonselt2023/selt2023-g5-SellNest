be6d280e4c64fe1f5d6988daabdf8ac0a42808f1
teckenrod <treyeck2@gmail.com>
2023-12-03 19:17:08 -0600

    now is_sold deafaults to false

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -13,4 +13,5 @@ class Listing < ActiveRecord::Base
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :is_sold, inclusion: { in: [true, false] }
 end
