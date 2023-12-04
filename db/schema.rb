bef82dff28dd02955414d39ef36cd09b48b57198
teckenrod <treyeck2@gmail.com>
2023-12-03 19:17:02 -0600

    now is_sold deafaults to false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_01_203004) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_04_011529) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -73,7 +73,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_01_203004) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.json "photos"
-    t.boolean "is_sold"
+    t.boolean "is_sold", default: false
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
