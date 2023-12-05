69b2940768a98df42d5a5f87790a278f21c8e13c
teckenrod <treyeck2@gmail.com>
2023-12-05 16:25:18 -0600

    reviews now created and shown

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,24 @@
 class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
+    @listing_id = params[:listing_id]
+    @user_id = current_user.id
 
     if @review.save
-      # success
+      redirect_to user_path(current_user)
     else
-      # failure
+      flash[:alert] = 'Error creating the review.'
+      render 'new'
     end
   end
 
+  def new
+    @review = Review.new
+    @listing_id = params[:listing_id]
+    @user_id = current_user.id
+  end
+
   def review_params
-    params.require(:review).permit(:content, :rating, :user_id, :listing_id)
+    params.require(:review).permit(:content, :rating, :listing_id, :user_id)
   end
 end
