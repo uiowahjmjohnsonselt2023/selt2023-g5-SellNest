947a46722ab76293b109e3a0fe2c2eca8f5938c0
teckenrod <treyeck2@gmail.com>
2023-12-05 13:30:19 -0600

    added review controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_04_011529) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_05_191931) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -96,6 +96,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_04_011529) do
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.text "content"
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "tags", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -130,4 +139,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_04_011529) do
   add_foreign_key "order_items", "listings"
   add_foreign_key "order_items", "orders"
   add_foreign_key "orders", "users"
+  add_foreign_key "reviews", "users"
 end
