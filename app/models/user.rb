b133ccb3f063710613b10d46a66920e60cf561c7
Tristan Pawlenty <tristan-pawlenty@uiowa.edu>
2023-12-04 21:20:49 -0600

    Added bookmark button to listings page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ActiveRecord::Base
   has_one :cart, dependent: :destroy
   has_many :orders
+  has_many :bookmarks, dependent: :destroy
   after_create :create_cart_for_user
   def seller?
     seller == true
+  end
+
+  def bookmarked?(listing)
+    bookmarks.exists?(listing_id: listing.id)
   end
 
   devise :omniauthable, :database_authenticatable, :registerable,
