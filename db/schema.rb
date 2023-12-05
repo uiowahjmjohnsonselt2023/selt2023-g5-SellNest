c7056d891de59492b531258697694e05a94f0192
Charles Wilmot II <chazwilmot@gmail.com>
2023-12-04 20:56:25 -0600

    heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_04_011529) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_04_014032) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_04_011529) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "listings_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["listings_id"], name: "index_bookmarks_on_listings_id"
+    t.index ["user_id"], name: "index_bookmarks_on_user_id"
   end
 
   create_table "cart_items", force: :cascade do |t|
@@ -121,6 +130,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_04_011529) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bookmarks", "listings", column: "listings_id"
+  add_foreign_key "bookmarks", "users"
   add_foreign_key "cart_items", "carts"
   add_foreign_key "cart_items", "listings"
   add_foreign_key "carts", "users"
