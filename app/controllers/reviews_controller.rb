b5c00ce5fb270bc8ddfac379d845f5a9a9d3a25e
teckenrod <treyeck2@gmail.com>
2023-12-05 13:44:15 -0600

    added listing reference to listing so we can connect to seller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,15 @@
 class ReviewsController < ApplicationController
+  def create
+    @review = Review.new(review_params)
+
+    if @review.save
+      # success
+    else
+      # failure
+    end
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating, :user_id, :listing_id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_05_191931) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_05_194256) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -102,6 +102,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_05_191931) do
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "listing_id", null: false
+    t.index ["listing_id"], name: "index_reviews_on_listing_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
@@ -139,5 +141,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_05_191931) do
   add_foreign_key "order_items", "listings"
   add_foreign_key "order_items", "orders"
   add_foreign_key "orders", "users"
+  add_foreign_key "reviews", "listings"
   add_foreign_key "reviews", "users"
 end
