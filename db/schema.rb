6f348a4a1f087bab84601df5a4beac215b3fcfd0
Charles Wilmot II <chazwilmot@gmail.com>
2023-12-04 21:30:33 -0600

    logic for notifications (waiting homepage css to be done)
    need to add notification bell

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_05_012601) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_05_032557) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -87,6 +87,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_05_012601) do
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
+  create_table "notifications", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.text "content"
+    t.boolean "read"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_notifications_on_user_id"
+  end
+
   create_table "order_items", force: :cascade do |t|
     t.integer "order_id", null: false
     t.integer "listing_id", null: false
@@ -139,6 +148,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_05_012601) do
   add_foreign_key "listing_tags", "listings"
   add_foreign_key "listing_tags", "tags"
   add_foreign_key "listings", "users"
+  add_foreign_key "notifications", "users"
   add_foreign_key "order_items", "listings"
   add_foreign_key "order_items", "orders"
   add_foreign_key "orders", "users"
