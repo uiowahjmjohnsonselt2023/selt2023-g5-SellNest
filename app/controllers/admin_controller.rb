3841b10e0e86bfdd90df87a9e2d20191888d23af
teckenrod <treyeck2@gmail.com>
2023-12-04 22:31:52 -0600

    added more admin stats

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,6 +3,7 @@ class AdminController < ApplicationController
     @total_users = User.count
     @total_sellers = User.where(seller: true).count
     @total_sales = Order.sum(:total)
+    @listed_merchandise = Listing.where(is_sold: false).sum(:price)
 
     @active_listings = Listing.where(is_sold: false).count
     @sold_listings = Listing.where(is_sold: true).count
