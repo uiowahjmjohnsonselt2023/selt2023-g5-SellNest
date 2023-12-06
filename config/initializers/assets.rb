71ac668058bce5d467bb724312f22d444359bf83
Charles Wilmot II <chazwilmot@gmail.com>
2023-12-06 15:50:00 -0600

    Home page styling reworking

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,7 @@ Rails.application.config.assets.version = "1.0"
 # config/initializers/assets.rb
 Rails.application.config.assets.precompile += %w( cart.css )
 Rails.application.config.assets.precompile += %w( chartkick.js )
+Rails.application.config.assets.precompile += %w( home.css )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
