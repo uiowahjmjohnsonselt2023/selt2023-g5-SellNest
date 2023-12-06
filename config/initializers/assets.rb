ecdc1879a6ba45b94daf6af431a2611e9a75b742
Charles Wilmot II <chazwilmot@gmail.com>
2023-12-06 16:02:36 -0600

    Homepage and Admin dashboard css updates

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,7 @@ Rails.application.config.assets.version = "1.0"
 Rails.application.config.assets.precompile += %w( cart.css )
 Rails.application.config.assets.precompile += %w( chartkick.js )
 Rails.application.config.assets.precompile += %w( home.css )
+Rails.application.config.assets.precompile += %w( admin.css )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
