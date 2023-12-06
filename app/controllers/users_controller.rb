8974c0162a786e6d208cbf3cd06a7a8b7b054459
Charlize Santos <charlize-santos@uiowa.edu>
2023-12-05 20:53:38 -0600

    Trying to fix google auth in heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,6 @@ class UsersController < ApplicationController
   def set_user
     @user = User.find(params[:id])
   end
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-    end
-  end
 
 
   def index
