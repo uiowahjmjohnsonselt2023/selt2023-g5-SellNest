64ee18640e47843ecf948e26bce9af5b62f100ed
Charlize Santos <charlize-santos@uiowa.edu>
2023-12-06 23:32:56 -0600

    adding tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+tags = Tag.create([{ name: 'Art & Crafts'}, { name: 'Beauty & Personal Care'}, { name: 'Books'}, { name: 'Clothing'},
+                   { name: 'Electronics'}, { name: 'Furniture'}, { name: 'Health & Fitness'}, { name: 'Home & Kitchen'},
+                   { name: 'Sports & Outdoors'}, { name: 'Toys & Games'}])
+
 admin = User.create!(
   email: 'admin@email',
   password: 'adminpass',
