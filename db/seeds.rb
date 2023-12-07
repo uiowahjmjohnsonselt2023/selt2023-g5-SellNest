2d4dfcf33644ea7440f3f36009964dda924e221f
Charlize Santos <charlize-santos@uiowa.edu>
2023-12-06 23:24:14 -0600

    Fixing Faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,32 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-tags = Tag.create([{ name: 'Art & Crafts'}, { name: 'Beauty & Personal Care'}, { name: 'Books'}, { name: 'Clothing'},
-                   { name: 'Electronics'}, { name: 'Furniture'}, { name: 'Health & Fitness'}, { name: 'Home & Kitchen'},
-                   { name: 'Sports & Outdoors'}, { name: 'Toys & Games'}])
-
-admin = User.create!(
-  email: 'admin@email',
-  password: 'adminpass',
-  seller: true
-)
-
-admin_listing = Listing.create!(
-  name: 'admin',
-  description: 'admin',
-  price: 0.0,
-  user: admin
-)
-
-users = []
-50.times do |i|
-  users << User.create!(
-    email: "user#{i + 3}@example.com",
-    password: 'password',
-    full_name: "User #{i + 3}",
-    seller: i.even?
-  )
-end
+# tags = Tag.create([{ name: 'Art & Crafts'}, { name: 'Beauty & Personal Care'}, { name: 'Books'}, { name: 'Clothing'},
+#                    { name: 'Electronics'}, { name: 'Furniture'}, { name: 'Health & Fitness'}, { name: 'Home & Kitchen'},
+#                    { name: 'Sports & Outdoors'}, { name: 'Toys & Games'}])
+#
+# admin = User.create!(
+#   email: 'admin@email',
+#   password: 'adminpass',
+#   seller: true
+# )
+#
+# admin_listing = Listing.create!(
+#   name: 'admin',
+#   description: 'admin',
+#   price: 0.0,
+#   user: admin
+# )
+#
+# users = []
+# 50.times do |i|
+#   users << User.create!(
+#     email: "user#{i + 3}@example.com",
+#     password: 'password',
+#     full_name: "User #{i + 3}",
+#     seller: i.even?
+#   )
+# end
 
 listings = []
 users.each do |user|
