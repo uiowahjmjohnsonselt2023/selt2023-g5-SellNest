fb32eb140e42aa6f591f86daa9a49d5144430178
Charles Wilmot II <chazwilmot@gmail.com>
2023-12-07 03:56:15 -0600

    create listings css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,8 @@ Rails.application.config.assets.precompile += %w( cart.css )
 Rails.application.config.assets.precompile += %w( chartkick.js )
 Rails.application.config.assets.precompile += %w( home.css )
 Rails.application.config.assets.precompile += %w( admin.css )
+Rails.application.config.assets.precompile += %w( create-listing.css )
+
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
