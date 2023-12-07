dcf6134f5e755ea996ece692bff96373b569f65e
Charlize Santos <charlize-santos@uiowa.edu>
2023-12-06 23:26:28 -0600

    Trying to fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-# tags = Tag.create([{ name: 'Art & Crafts'}, { name: 'Beauty & Personal Care'}, { name: 'Books'}, { name: 'Clothing'},
-#                    { name: 'Electronics'}, { name: 'Furniture'}, { name: 'Health & Fitness'}, { name: 'Home & Kitchen'},
-#                    { name: 'Sports & Outdoors'}, { name: 'Toys & Games'}])
-#
-# admin = User.create!(
-#   email: 'admin@email',
-#   password: 'adminpass',
-#   seller: true
-# )
-#
-# admin_listing = Listing.create!(
-#   name: 'admin',
-#   description: 'admin',
-#   price: 0.0,
-#   user: admin
-# )
-#
-# users = []
-# 50.times do |i|
-#   users << User.create!(
-#     email: "user#{i + 3}@example.com",
-#     password: 'password',
-#     full_name: "User #{i + 3}",
-#     seller: i.even?
-#   )
-# end
+admin = User.create!(
+  email: 'admin@email',
+  password: 'adminpass',
+  seller: true
+)
+
+admin_listing = Listing.create!(
+  name: 'admin',
+  description: 'admin',
+  price: 0.0,
+  user: admin
+)
+
+users = []
+50.times do |i|
+  users << User.create!(
+    email: "user#{i + 3}@example.com",
+    password: 'password',
+    full_name: "User #{i + 3}",
+    seller: i.even?
+  )
+end
 
 listings = []
 users.each do |user|
