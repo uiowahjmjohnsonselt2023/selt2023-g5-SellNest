a14bd66d5217199cdcac4d2e1f1cd9b9b04aa203
Charles Wilmot II <chazwilmot@gmail.com>
2023-12-07 19:09:21 -0600

    admin dashboard analytics

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -21,5 +21,9 @@ class AdminController < ApplicationController
     end
 
     @tagged_listings = Tag.joins(:listings).group('tags.name').count
-  end
+    @new_users_over_time = User.group_by_day(:created_at).count
+    @total_sales_by_month = Order.where(status: 'complete').group_by_month(:created_at, format: "%b %Y").sum(:total)
+    @sales_by_category = OrderItem.joins(:order, listing: :tags).where(orders: { status: 'complete' }).group_by_month('orders.created_at', format: "%b %Y").group('tags.name').sum('quantity * listings.price')
+    @price_vs_rating = Listing.joins(:reviews).select('listings.price', 'reviews.rating').map { |l| { x: l.price, y: l.rating } }  end
+    @listings_per_day = Listing.group_by_day(:created_at).count
 end
