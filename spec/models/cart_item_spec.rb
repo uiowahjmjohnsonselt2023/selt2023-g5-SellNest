0e333481d7f3c3a122f5663e74118e227fcdc8db
Tristan Pawlenty <tristan-pawlenty@uiowa.edu>
2023-12-07 20:20:17 -0600

    Modified tests for cart item

--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe CartItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to belong_to(:cart) }
+    it { is_expected.to belong_to(:listing) }
+  end
+
+  describe 'before_create set_default_quantity' do
+    it 'sets default quantity to 1 before creation' do
+      cart_item = CartItem.create
+      expect(cart_item.quantity).to eq(1)
+    end
+
+    it 'does not override quantity if already set' do
+      cart_item = CartItem.create(quantity: 5)
+      expect(cart_item.quantity).to eq(5)
+    end
+  end
 end
