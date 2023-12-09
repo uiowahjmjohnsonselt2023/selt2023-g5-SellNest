9f73ead59350adc4fe2bf84b65ae90947f0ac502
Tristan Pawlenty <tristan-pawlenty@uiowa.edu>
2023-12-08 20:37:20 -0600

    Modified listing tag

--- spec/models/listing_tag_spec.rb
+++ spec/models/listing_tag_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe ListingTag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to belong_to(:listing) }
+    it { is_expected.to belong_to(:tag) }
+  end
+
+  # Add more tests as needed for validations or any custom methods in ListingTag model
 end
